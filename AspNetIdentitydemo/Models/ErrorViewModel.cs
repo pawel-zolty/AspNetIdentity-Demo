fe1ce912dd96922379e3d3111b73f4ccf260577b
pawel-zolty <zolty1902@gmail.com>
2022-03-01 00:08:21 +0100

    add BE classes for MVC

---
+++ AspNetIdentitydemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetIdentitydemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
