fbd51dbbe6aff1d02c0e7ea33fbfdaea4aebd05c
pzoltaniecki <pawel.zoltaniecki@insert.com.pl>
2022-03-02 09:28:03 +0100

    ensure organization for google auth

--- AspNetIdentitydemo/Controllers/ExternalController.cs
+++ AspNetIdentitydemo/Controllers/ExternalController.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -11,14 +13,18 @@ namespace AspNetIdentitydemo.Controllers
 {
     public class ExternalController : Controller
     {
+        private static string SCHEME = IdentityConstants.ExternalScheme;
         private readonly UserManager<MyCustomUser> _userManager;
         private readonly IUserClaimsPrincipalFactory<MyCustomUser> _claimsPrincipalFactory;
+        private readonly MyCustomUserDbContext _dbContext;
 
         public ExternalController(UserManager<MyCustomUser> userManager,
-            IUserClaimsPrincipalFactory<MyCustomUser> claimsPrincipalFactory)
+            IUserClaimsPrincipalFactory<MyCustomUser> claimsPrincipalFactory,
+            MyCustomUserDbContext dbContext)
         {
             _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
             _claimsPrincipalFactory = claimsPrincipalFactory ?? throw new ArgumentNullException(nameof(claimsPrincipalFactory));
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
         }
 
         [HttpGet]
@@ -34,10 +40,9 @@ namespace AspNetIdentitydemo.Controllers
         }
 
         [HttpGet]
-        //[Route("/signin-google")]
         public async Task<IActionResult> LoginCallback()
         {
-            var result = await HttpContext.AuthenticateAsync(IdentityConstants.ExternalScheme);
+            var result = await HttpContext.AuthenticateAsync(SCHEME);
             var externalUserId = result.Principal.FindFirstValue("sub")
                 ?? result.Principal.FindFirstValue(ClaimTypes.NameIdentifier)
                 ?? throw new Exception("Cannot find external id");
@@ -54,12 +59,16 @@ namespace AspNetIdentitydemo.Controllers
                 {
                     user = await _userManager.FindByEmailAsync(email);
 
+                    var externalOrganizationId = 2;
+                    var organization = await EnsureOrganization(externalOrganizationId);
+
                     if (user == null)
                     {
                         user = new MyCustomUser
                         {
                             UserName = email,
                             Email = email,
+                            OrganizationId = organization.Id
                         };
 
                         await _userManager.CreateAsync(user);
@@ -73,9 +82,27 @@ namespace AspNetIdentitydemo.Controllers
 
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
             var claimsPrincipal = await _claimsPrincipalFactory.CreateAsync(user);
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+            await HttpContext.SignInAsync(SCHEME, claimsPrincipal);
 
             return RedirectToAction("Index", "Home");
+        }
+
+        private async Task<Organization> EnsureOrganization(int externalId)
+        {
+            var organization = await _dbContext.Organizations.Where(o => o.ExternalId == externalId).SingleOrDefaultAsync();
+
+            if (organization == null)
+            {
+                organization = new Organization
+                {
+                    Name = "Google Inc.",
+                    ExternalId = externalId
+                };
+                _dbContext.Organizations.Add(organization);
+                await _dbContext.SaveChangesAsync();
+            }
+
+            return organization;
         }
     }
 }
